28088a684ed5e8e53d93b419ed11d1a06c1ee29e
Krzysiek <krzyspmac@gmail.com>
2017-05-03 22:53:29 -0400

    Copied sonar measure code to a new file.
    Original kept unchanged.

--- SonarMeasure.py
+++ SonarMeasure.py
@@ -3,47 +3,64 @@ import RPi.GPIO as GPIO
 
 class SonarMeasure(object):
 
-	# keep variables
-	_pin = None
-	_startTime = None
-	_dinstance = None # store for the future
+    # Vars
+    _pins = None
+    
+    # Constructor
+    def __init__(self, *pins):
+        self._pins = pins
+        self.init()
+        pass
+        
+    def init(self):
+        # Use BCM GPIO references
+        # instead of physical pin numbers
+        GPIO.setmode(GPIO.BCM)
 
-	# Initialize with a pin number
-	def __init__(self, pin):
-		self._pin = pin
+        # Set pins as output and input
+        for pin in self._pins:
+            GPIO.setup(pin,GPIO.OUT)  # Initial state as output
+            pass
+        
+        # Set trigger to False (Low)
+        for pin in self._pins:
+           GPIO.output(pin, False)
+            pass
 
- 		GPIO.setup(self._pin,GPIO.OUT)
-  		GPIO.output(self._pin, True)
-  		time.sleep(0.00001)
-  		GPIO.output(self._pin, False)
+        print "Waiting For Sensor To Settle"
+        time.sleep(2)
+        pass
+        
+    def get_measures(self):
+        array = []
+        for pin in self._pins:
+            GPIO.output(pin, False)
+            array.append(pin)
+            pass
+        return tuple(array)
+        
+    def get_measure_single(PIN):
+    
+        # This function measures a distance
+        # Pulse the trigger/echo line to initiate a measurement
+        GPIO.setup(PIN,GPIO.OUT)  # Initial state as output
+        GPIO.output(PIN, True)
+        time.sleep(0.00001)
+        GPIO.output(PIN, False)
+        #ensure start time is set in case of very quick return
+        start = time.time()
 
-		#ensure start time is set in case of very quick return
-		self._startTime = time.time()
-
-		# set line to input to check for start of echo response
-		GPIO.setup(self._pin, GPIO.IN)
-		pass # end constructor
-		
-	def doWork(self):
-		# check if it settled
-		if GPIO.input(self._pin)==0:
-			self._startTime = time.time()
-			pass
-
-		# check if it calcualted
-		# if YES - return appropriate tuple
-		# if NO - return a tuple marked as `not ready'
-		if GPIO.input(self._pin)==1:
-			stop = time.time()
-			elapsed = stop - self._startTime
-			self._distance = elapsed * 17150
-			return (True, self._distance)
-			pass
-		else:
-			return (False, -1)
-			pass
-
-		pass # end doWork
-	
-	pass # end SonarMeasure
+        # set line to input to check for start of echo response
+        GPIO.setup(PIN, GPIO.IN)
+        while GPIO.input(PIN)==0:
+            start = time.time()
+            
+        # Wait for end of echo response
+        while GPIO.input(PIN)==1:
+            stop = time.time()
+  
+        elapsed = stop-start
+        distance = elapsed * 17150
+        return distance
+        pass
 
