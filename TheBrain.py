ea6e348ee86ee354a7dc64f2f81ed77ffd063a24
Krzysiek <krzyspmac@gmail.com>
2017-05-03 22:54:09 -0400

    Initialization
    Reading params from config file + args parsing.

--- TheBrain.py
+++ TheBrain.py
@@ -1,31 +1,61 @@
 import RPi.GPIO as GPIO
+import time
+import ConfigParser
+import argparse
 from SonarMeasure import SonarMeasure
 
 class TheBrain(object):
-	# vars
-	_sonar1 = None
-
-	def __init__(self):
-		GPIO.setmode(GPIO.BCM)
-
-		self._sonar1 = SonarMeasure(23)
-		pass # end constructor
-
-	def run(self):
-		print "Starting"
-		while (True):
-			print "Loop"
-			sonar1Result = self._sonar1.doWork()
-			if sonar1Result[0] == True:
-				print sonar1Result[1]
-				pass
-			else:
-				print "Waiting to settle"
-				pass
-			pass # end loop
-		pass # end run	
-
-	pass # end class
+    # vars
+    _sonar = None
+    
+    _pinSonar0 = 23
+    _pinSonar1 = 24
+    _pinSonar2 = 25
+    
+    def __init__(self):
+        print "Initializing"
+        
+        config_name = None
+        
+        # parse command line arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', metavar='-C', default='config.robot', type=str, help='point to a config file')
+        args = parser.parse_args()
+        config_name = args.config
+        
+        # parse config file
+        try:
+            config = ConfigParser.ConfigParser()
+            config.readfp(open(config_name))
+            print "Using config file: '" + config_name + "'"
+        
+            self._pinSonar0 = config.getint('SONAR', 'PinSonar0')
+            self._pinSonar1 = config.getint('SONAR', 'PinSonar1')
+            self._pinSonar2 = config.getint('SONAR', 'PinSonar2')
+            pass
+        except:
+            print "Not using a config file"
+            pass
+        
+        self._sonar = SonarMeasure(self._pinSonar0, self._pinSonar1, self._pinSonar2)
+        pass
+        
+    def run(self):
+        print "Staring"
+        
+        try:
+            while (True):
+                print self._sonar.get_measures()
+                time.sleep(2)
+                
+                pass # end loop
+            pass # end run  
+        except KeyboardInterrupt:
+            # User pressed CTRL-C
+            # Reset GPIO settings
+             GPIO.cleanup()
+            pass
+    pass # end class
 
 brain = TheBrain()
 brain.run()
---
+++ config.robot
@@ -0,0 +1,7 @@
+[DEFAULT]
+
+[SONAR]
+PinSonar0 = 23
+PinSonar1 = 24
+PinSonar2 = 25
+
